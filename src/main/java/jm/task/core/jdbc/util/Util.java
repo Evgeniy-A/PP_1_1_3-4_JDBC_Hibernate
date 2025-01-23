91bcbe0573425cd2ce69d91ff5765f8e84d9af92
Evgeniy <Antonov.174.rus@mail.ru>
2025-01-23 14:24:14 +0500

    task 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,14 +15,41 @@ public class Util {
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
 
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             System.out.println("Connection OK");
-        } catch  (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return connection;
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.setProperty("hibernate.connection.url", DB_URL);
+                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                configuration.setProperty("hibernate.connection.username", "root");
+                configuration.setProperty("hibernate.connection.password", "root");
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                configuration.setProperty("show_sql", "true");
+
+                //configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+                configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                throw new ExceptionInInitializerError(e);
+            }
+        }
+        return sessionFactory;
     }
 }
